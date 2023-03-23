bf59361e6ef647e2a937dd0eb2c5e6413964388b
José Dias <devjosedias@gmail.com>
2023-03-23 16:23:33 -0300

    Alteração de layout mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
 const cadastro = document.getElementById("popup")
-const login = document.getElementById("login")
+const principal = document.querySelector(".principal")
 const popup = document.getElementById("abrir-popup")
+
 
 function abrirCadastro(){
     popup.classList.add("remover")
-    login.classList.add("remover")
+    principal.classList.add("remover")
     cadastro.classList.add("popup")
 }
 
 function fecharCadastro(){
     popup.classList.remove("remover")
-    login.classList.remove("remover")
     cadastro.classList.remove("popup")
+    principal.classList.remove("remover")
 }
